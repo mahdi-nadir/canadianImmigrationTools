32e11504bb8e9c232571f5f23e94b800d6f5d61f
Mahdi NADIR <e2194498@cmaisonneuve.qc.ca>
2023-08-16 23:38:58 -0400

    add night icons and logic for sunrise, sunset and current time according to timezone

--- assets/script/weatherClass.js
+++ assets/script/weatherClass.js
@@ -1,5 +1,6 @@
 import weatherapi from './apikey.js';
 
+
 export default class WeatherClass {
     constructor() {
         this.modalResult = document.querySelector('#modalResult');
@@ -61,35 +62,76 @@ export default class WeatherClass {
         try {
             const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},CA&appid=${this.API_KEY[Math.floor(Math.random() * this.API_KEY.length)]}&units=metric`);
             const json = await response.json();
+
+
+            const timezoneOffset = json.timezone / 3600;
+
+            const currentTime = new Date();
+            currentTime.setHours(currentTime.getUTCHours() + timezoneOffset);
+            const currentHours = currentTime.getHours();
+            const currentMinutes = currentTime.getMinutes();
+
+            const sunriseTime = new Date(json.sys.sunrise * 1000);
+            sunriseTime.setHours(sunriseTime.getUTCHours() + timezoneOffset);
+            const sunriseHour = sunriseTime.getHours();
+            const sunriseMinute = sunriseTime.getMinutes();
+
+            const sunsetTime = new Date(json.sys.sunset * 1000);
+            sunsetTime.setHours(sunsetTime.getUTCHours() + timezoneOffset);
+            const sunsetHour = sunsetTime.getHours();
+            const sunsetMinute = sunsetTime.getMinutes();
+
             this.temperature = Math.round(parseFloat(json.main.temp))
             this.humidity = json.main.humidity;
             this.windSpeed = json.wind.speed;
-            // this.weatherIcon = json.weather[0].icon;
-            this.condition = json.weather[0].main;
+
+            if (Date.now() > json.sys.sunrise * 1000 && Date.now() < json.sys.sunset * 1000) {
+                this.condition = json.weather[0].main;
+                this.condition == 'Clouds' ? this.condition = 'Cloudy' : this.condition;
+            } else {
+                this.condition = json.weather[0].main + ' night';
+                this.condition == 'Clouds night' ? this.condition = 'Cloudy night' : this.condition;
+            }
             this.result.innerHTML = `
-            <div class="flex flex-col items-center justify-center gap-8">
+            <div class="flex flex-col items-center justify-center gap-4">
                 <div class="flex flex-col items-center justify-center">
-                    <p class="text-3xl font-bold">${this.city.charAt(0).toUpperCase() + this.city.slice(1)}</p>
                     <img src="assets/images/weather_icons/${this.condition}.png" alt="weather icon" class="w-20 md:w-30 h-20 md:h-30 mt-1">
+                    <p class="text-2xl font-bold">${this.city.charAt(0).toUpperCase() + this.city.slice(1)}</p>
                     <div class="flex flex-row items-center justify-between gap-6">
-                        <p class="text-2xl font-bold" id="degree">${this.temperature}°C</p>
-                        <p class="text-2xl font-bold">${this.condition}</p>
+                        <p class="text-2xl" id="degree">${this.temperature}°C</p>
+                        <p class="text-2xl">${this.condition.split(' ')[0]}</p>
                     </div>
                 </div>
-                <div class="flex flex-row items-center justify-center gap-6 md:gap-10">
-                    <div class="flex flex-row items-center justify-center">
-                        <img src="assets/images/weather_icons/humidity.png" alt="weather icon" class="w-5 md:w-8 h-5 md:h-8">
-                        <div class="flex flex-col items-center justify-center">
-                            <p class="text-sm font-bold">${this.humidity}%</p>
-                            <p class="text-sm">Humidity</p>
+                <div class="flex flex-col items-center justify-center gap-2 border-t-2 border-gray-50">
+                    <div class="flex flex-row items-center justify-center gap-6 md:gap-10 border-b-2 border-gray-50">
+                        <div class="flex flex-row items-center justify-center">
+                            <img src="assets/images/weather_icons/sunrise.png" alt="weather icon" class="w-5 md:w-8 h-5 md:h-8">
+                            <p class="text-sm font-bold">${String(sunriseHour).padStart(2, '0')}:${String(sunriseMinute).padStart(2, '0')}</p>
+                        </div>
+                        <div class="flex flex-row items-center justify-center gap-1">
+                            <img src="assets/images/weather_icons/clock.png" alt="weather icon" class="w-4 h-4">
+                            <p class="text-sm font-bold">${String(currentHours).padStart(2, '0')}:${String(currentMinutes).padStart(2, '0')}</p>
+                        </div>
+                        <div class="flex flex-row items-center justify-center">
+                            <img src="assets/images/weather_icons/sunset.png" alt="weather icon" class="w-5 md:w-8 h-5 md:h-8">
+                            <p class="text-sm font-bold">${String(sunsetHour).padStart(2, '0')}:${String(sunsetMinute).padStart(2, '0')}</p>
                         </div>
                     </div>
-                    <div class="flex flex-row items-center justify-center gap-2">
-                        <div class="flex flex-col items-center justify-center">
-                            <p class="text-sm font-bold">${this.windSpeed} km/h</p>
-                            <p class="text-sm">Wind Speed</p>
+                    <div class="flex flex-row items-center justify-center gap-6 md:gap-10">
+                        <div class="flex flex-row items-center justify-center">
+                            <img src="assets/images/weather_icons/humidity.png" alt="weather icon" class="w-5 md:w-8 h-5 md:h-8">
+                            <div class="flex flex-col items-center justify-center">
+                                <p class="text-sm font-bold">${this.humidity}%</p>
+                                <p class="text-sm">Humidity</p>
+                            </div>
                         </div>
-                        <img src="assets/images/weather_icons/winnd.png" alt="weather icon" class="w-5 md:w-8 h-6 md:h-10">
+                        <div class="flex flex-row items-center justify-center">
+                            <div class="flex flex-col items-center justify-center">
+                                <p class="text-sm font-bold">${this.windSpeed} km/h</p>
+                                <p class="text-sm">Wind Speed</p>
+                            </div>
+                            <img src="assets/images/weather_icons/winnd.png" alt="weather icon" class="w-5 md:w-8 h-6 md:h-10">
+                        </div>
                     </div>
                 </div>
             </div>
